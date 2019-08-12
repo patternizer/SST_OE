1746cabf457faf9e630c2e4e773ece4db30e5cda
Michael Taylor <patternizer@gmail.com>
2019-08-13 00:09:25 +0100

    Conda environment activation code

---
+++ inmyvenv.sh
@@ -0,0 +1,10 @@
+#. /gws/nopw/j04/esacci_sst/software/miniconda3/etc/profile.d/conda.sh
+. /gws/nopw/j04/fiduceo/Users/mtaylor/anaconda3/etc/profile.d/conda.sh
+export PATH=$PATH
+conda activate mike
+export OMP_NUM_THREADS=1
+export USE_SIMPLE_THREADED_LEVEL3=1
+$@
+
+
+
